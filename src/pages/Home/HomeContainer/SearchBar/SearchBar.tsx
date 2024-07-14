bb7a30027cf2aa441d66a90df3813976f6fde702
isalim079 <salimreza1025@gmail.com>
2024-07-14 10:49:42 +0600

    search bar created

--- src/pages/Home/HomeContainer/SearchBar/SearchBar.tsx
+++ src/pages/Home/HomeContainer/SearchBar/SearchBar.tsx
@@ -1,8 +1,20 @@
+import { CgSearch } from "react-icons/cg";
 
 const SearchBar = () => {
     return (
-        <div>
-            
+        <div className="max-w-screen-xl mx-auto">
+            <div className="relative my-16">
+                <input
+                    className=" rounded-md w-full py-4 bg-backgroundLightGreen shadow-md px-6 focus:outline-textGreen/50"
+                    placeholder="Search your plants..."
+                    type="search"
+                    name=""
+                    id=""
+                />
+                <button className="bg-backgroundLightGreen p-1 shadow-md absolute top-[27%] right-5 text-2xl  rounded-full text-textGreen text-center flex justify-center items-center">
+                    <CgSearch className="" />
+                </button>
+            </div>
         </div>
     );
 };
